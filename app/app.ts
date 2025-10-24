af2385dac542810da50686d867b049c758278358
Alex <A.ochnev090@yandex.ru>
2025-10-24 22:49:35 +0500

    первый комит в ветке main

---
+++ app/app.ts
@@ -0,0 +1,8 @@
+class User {
+    public Name: string;
+    public Surname: string;
+    constructor(name: string, surname: string) {
+        this.Name = name;
+        this.Surname = surname;
+    }
+}
